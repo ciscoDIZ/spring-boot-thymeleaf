440e413cdc9e2bc77a353d93647c844ffdfe222f
ciscodiz <ciscoadiz@gmail.com>
2020-12-14 14:49:33 +0000

    v0.1 solucionado problema avatar usuario

--- src/main/java/net/openwebinars/proyectothymeleaf/ProyectothymeleafApplication.java
+++ src/main/java/net/openwebinars/proyectothymeleaf/ProyectothymeleafApplication.java
@@ -1,7 +1,10 @@
 package net.openwebinars.proyectothymeleaf;
 
+import net.openwebinars.proyectothymeleaf.upload.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProyectothymeleafApplication {
@@ -9,5 +12,11 @@ public class ProyectothymeleafApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProyectothymeleafApplication.class, args);
     }
-
+    @Bean
+    CommandLineRunner init(StorageService storageService){
+        return (args) ->{
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
 }
